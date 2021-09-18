adb2d9fd67b79dd7b6bd73df023238367809af19
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-09-18 09:05:34 -0500

    feat: Estructura del Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <img className="header__img" src="./assets/img/kisspng.png" alt="logo" />
+    <div className="header__menu">
+      <div className="header__menu--profile">
+        <img src="./assets/img/user.png" alt="user" />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a href="./assets/componentes/not-found.html">Cuenta</a></li>
+        <li><a href="./assets/componentes/sign-in.html">Cerrar sesión</a></li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
