fb03b05c5455c5d339ed81649ab1919d0308afb7
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-09-21 16:38:48 -0500

    feat: Container: 404 Not Found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <>
+    <section className="error-container">
+      <span>4</span>
+      <span><span className="screen-reader-text">0</span></span>
+      <span>4</span>
+    </section>
+  </>
+);
+
+export default NotFound;
