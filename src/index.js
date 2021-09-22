773cc14998b9c9a2f3b6be7e64fca3612f3103f8
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-09-22 10:43:40 -0500

    feat: Instalación de Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 import './assets/styles/App.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
+
