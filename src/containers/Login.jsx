19ab0eb5ccc62833868a529b5101f0eab2c6b9bd
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-09-23 09:34:44 -0500

    feat: Crear Formulario de Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,50 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-.png';
 import facebookIcon from '../assets/static/facebook-.png';
 import '../assets/styles/components/Login.scss';
 
-const Login = () => (
-  <section className="login">
-    <section className="login__container">
-      <h2>Inicia sesión</h2>
-      <form action="/index.html" className="login__container--form">
-        <input className="input-login" aria-label="correo" type="text" placeholder="Correo" />
-        <input className="input-login" aria-label="contraseña" type="password" placeholder="Contraseña" />
-        <button className="button" type="button">
-          <Link to="/">
-            Iniciar sesión
-          </Link>
-        </button>
-        <div className="login__container--remember-me">
-          <label>
-            <input type="checkbox" name="" id="cbox1" value="checkbox" />
-            Recuerdame
-          </label>
-          <a href="#">Olvide mi contraseña</a>
-        </div>
-      </form>
+const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
 
-      <section className="login__container--social-media">
-        <div>
-          <img src={googleIcon} alt="Google" />
-          Inicia sesión con Google
-        </div>
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-        <div>
-          <img src={facebookIcon} alt="Facebook" />
-          Inicia sesión con Facebook
-        </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="login">
+      <section className="login__container">
+        <h2>Inicia sesión</h2>
+        <form className="login__container--form" onSubmit={handleSubmit}>
+          <input
+            name="email"
+            className="input-login"
+            aria-label="correo"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input-login"
+            aria-label="contraseña"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button" type="button">
+            <Link to="/">Iniciar sesión</Link>
+          </button>
+          <div className="login__container--remember-me">
+            <label>
+              <input type="checkbox" name="" id="cbox1" value="checkbox" />
+              Recuerdame
+            </label>
+            <a href="#">Olvide mi contraseña</a>
+          </div>
+        </form>
+
+        <section className="login__container--social-media">
+          <div>
+            <img src={googleIcon} alt="Google" />
+            Inicia sesión con Google
+          </div>
+
+          <div>
+            <img src={facebookIcon} alt="Facebook" />
+            Inicia sesión con Facebook
+          </div>
+        </section>
+
+        <p className="login__container--register">
+          No tienes ninguna cuenta
+          <Link to="/register">Registrate</Link>
+        </p>
       </section>
-
-      <p className="login__container--register">
-        No tienes ninguna cuenta
-        <Link to="/register">
-          Registrate
-        </Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
 export default Login;
