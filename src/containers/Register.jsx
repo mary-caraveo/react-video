4c70dda2cc7e5568cbd8ecf250e9b396ce1e2c96
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-09-24 12:25:46 -0500

    feat: Register con Redux

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { registerRequest } from '../actions';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => {
+const Register = (props) => {
   const [form, setValues] = useState({
     email: '',
     name: '',
@@ -18,6 +20,8 @@ const Register = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
   };
 
   return (
@@ -57,4 +61,9 @@ const Register = () => {
     </section>
   );
 };
-export default Register;
+
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,28 @@
 const reducer = (state, action) => {
   const itemId = state.myList.find((items) => items.id === action.payload.id);
   switch (action.type) {
-    case 'SET_FAVORITE':
+    case "SET_FAVORITE":
       if (itemId) return state;
       return {
         ...state,
         myList: [...state.myList, action.payload],
       };
-    case 'DELETE_FAVORITE':
+    case "DELETE_FAVORITE":
       return {
         ...state,
         myList: state.myList.filter((items) => items.id !== action.payload),
       };
-    case 'LOGIN_REQUEST':
+    case "LOGIN_REQUEST":
       return {
         ...state,
         user: action.payload,
       };
-    case 'LOGOUT_REQUEST':
+    case "LOGOUT_REQUEST":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'REGISTER_REQUEST':
       return {
         ...state,
         user: action.payload,
